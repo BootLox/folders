979f41f90417980ba703254a847b0d5b823c096e
Jared <jarebeargamer@yahoo.com>
2018-02-17 03:44:12 +0000

    Window sized

--- collaborative_sketch/main.js
+++ collaborative_sketch/main.js
@@ -14,7 +14,7 @@ var pointsData = firebase.database().ref();
 var points = [];
 
 function setup() {
-   var canvas = createCanvas(400, 400);
+   var canvas = createCanvas(windowWidth, windowHeight);
   background(255);
   fill(0);
  
